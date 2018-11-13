6c0c1514f8266e2a96cd69b920036ab3bdf82b42
Summer Xia <summer@MacBook-Pro-2nd-gen.local>
2018-11-13 12:37:35 -0800

    new file:   .gitignore   new file:   main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#coding: utf8
+#author: Tian Xia (summer.xia1@pactera.com)
+
+from algorithm_3x import *
+
+if __name__ == "__main__":
+  os.system("clear")
+
+  parser = optparse.OptionParser(usage="cmd [optons] ..]")
+  #parser.add_option("-q", "--quiet", action="store_true", dest="verbose",
+                     #default=False, help="")
+  (options, args) = parser.parse_args()
+
