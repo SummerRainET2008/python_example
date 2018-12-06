d8ea641ef972cc34460ee4547973f30c3b404625
Tian Xia <summer@MacBook-Pro-2nd-gen.local>
2018-12-05 18:41:11 -0800

    deleted:    examples/sound/Common.py   modified:   examples/sound/ExamAudioInfo.py

--- examples/sound/ExamAudioInfo.py
+++ examples/sound/ExamAudioInfo.py
@@ -2,11 +2,9 @@
 #coding: utf8
 #author: Tian Xia (summer.xia1@pactera.com)
 
-from algorithm_3x import *
 from mutagen.mp3 import MP3
 from mutagen.flac import *
-import examples.sound.Common as music_common
-import Common as nlp_common
+from Common import *
 from multiprocessing import Pool
 import audioread
 
@@ -20,8 +18,15 @@ AUDIO_EXTENSIONS = [
 STATUS_ERROR          = -1
 STATUS_UNKNOWN        = -3
 
+def seconds_to_str(seconds: float):
+  hours = seconds // 3600
+  minutes = (seconds - hours * 3600) // 60
+  seconds = int(seconds - hours * 3600 - minutes * 60)
+
+  return f"{hours} h {minutes} m {seconds} s"
+
 def get_music_length(in_file: str):
-  ext = nlp_common.get_file_extension(in_file)
+  ext = get_file_extension(in_file)
   try:
     if ext == "mp3":
       audio = MP3(in_file)
@@ -31,7 +36,8 @@ def get_music_length(in_file: str):
         return get_music_length(out_file)
 
       cmd = f"ffmpeg -i {in_file} {out_file}"
-      if executeCmd(cmd) == 0:
+      code = execute_cmd(cmd)
+      if code == 0:
         return audio.info.length
       else:
         return STATUS_ERROR
@@ -50,11 +56,13 @@ def get_music_length(in_file: str):
         return get_music_length(out_file)
 
       cmd = f"sox {in_file} {out_file}"
-      if executeCmd(cmd) == 0:
+      code = execute_cmd(cmd)
+      if code == 0:
         return get_music_length(out_file)
 
       cmd = "sph2pipe -f rif {file_name} {out_file}"
-      if executeCmd(cmd) == 0:
+      code = execute_cmd(cmd)
+      if code == 0:
         return get_music_length(out_file)
 
       return STATUS_ERROR
@@ -67,17 +75,15 @@ def get_music_length(in_file: str):
     return STATUS_ERROR
 
 if __name__ == "__main__":
-  os.system("clear")
-
   parser = optparse.OptionParser(usage="cmd [optons] ..]")
   #parser.add_option("-q", "--quiet", action="store_true", dest="verbose",
                      #default=False, help="")
   parser.add_option("--folder")
   (options, args) = parser.parse_args()
 
-  file_names = nlp_common.get_files_in_folder(options.folder,
-                                              file_exts=AUDIO_EXTENSIONS,
-                                              resursive=True)
+  file_names = get_files_in_folder(options.folder,
+                                   file_extensions=AUDIO_EXTENSIONS,
+                                   resursive=True)
   file_names = list(file_names)
   print(f"There are {len(file_names)} files found.")
 
@@ -85,21 +91,26 @@ if __name__ == "__main__":
 
   total_seconds = defaultdict(float)
   error_file_num = 0
+  error_files = []
   for idx, (file_name, seconds) in enumerate(zip(file_names, durations)):
     if seconds < 0:
       if seconds in [STATUS_ERROR, STATUS_UNKNOWN]:
         error_file_num += 1
+        error_files.append(file_name)
 
       continue
 
-    time_str = music_common.seconds_to_str(seconds)
+    time_str = seconds_to_str(seconds)
     # print(f"'{idx}:{file_name}': {time_str}")
 
-    total_seconds[nlp_common.get_file_extension(file_name)] += seconds
+    total_seconds[get_file_extension(file_name)] += seconds
 
   print(f"#exception files: {error_file_num}")
+  print("\n".join(error_files), file=open("errors.txt", "w"))
+
+  print(f"There are {len(file_names)} files found.")
   for ext in total_seconds:
     seconds = total_seconds[ext]
-    time_str = music_common.seconds_to_str(seconds)
+    time_str = seconds_to_str(seconds)
     print(f"type='{ext}', #time: {time_str}")
 
