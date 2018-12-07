54d443789d12e83b21300325a7cdafa75d6798a1
Tian Xia <summer@MacBook-Pro-2nd-gen.local>
2018-12-06 20:23:37 -0800

    deleted:    examples/sound/102-129232-0009.flac   deleted:    examples/sound/AaronHuey_2010X.sph   modified:   examples/sound/ExamAudioInfo.py   deleted:    examples/sound/canon.mp3

--- examples/sound/ExamAudioInfo.py
+++ examples/sound/ExamAudioInfo.py
@@ -7,72 +7,14 @@ from mutagen.flac import *
 from Common import *
 from multiprocessing import Pool
 import audioread
+import typing
+from audio.AudioHelper import AudioHelper
 
-AUDIO_EXTENSIONS = [
-  "mp3",      # converted to wav
-  "flac",
-  "wav",
-  "sph",      # converted to wav
-]
+def convert(in_file: str):
+  return in_file, AudioHelper.convert_to_flac(in_file)
 
-STATUS_ERROR          = -1
-STATUS_UNKNOWN        = -3
-
-def seconds_to_str(seconds: float):
-  hours = seconds // 3600
-  minutes = (seconds - hours * 3600) // 60
-  seconds = int(seconds - hours * 3600 - minutes * 60)
-
-  return f"{hours} h {minutes} m {seconds} s"
-
-def get_music_length(in_file: str):
-  ext = get_file_extension(in_file)
-  try:
-    if ext == "mp3":
-      audio = MP3(in_file)
-
-      out_file = in_file + ".wav"
-      if os.path.exists(out_file):
-        return get_music_length(out_file)
-
-      cmd = f"ffmpeg -i {in_file} {out_file}"
-      code = execute_cmd(cmd)
-      if code == 0:
-        return audio.info.length
-      else:
-        return STATUS_ERROR
-
-    elif ext == "flac":
-      audio = FLAC(in_file)
-      return audio.info.length
-
-    elif ext == "wav":
-      audio = audioread.audio_open(in_file)
-      return audio.duration
-
-    elif ext == "sph":
-      out_file = f"{in_file}.wav"
-      if os.path.exists(out_file):
-        return get_music_length(out_file)
-
-      cmd = f"sox {in_file} {out_file}"
-      code = execute_cmd(cmd)
-      if code == 0:
-        return get_music_length(out_file)
-
-      cmd = "sph2pipe -f rif {file_name} {out_file}"
-      code = execute_cmd(cmd)
-      if code == 0:
-        return get_music_length(out_file)
-
-      return STATUS_ERROR
-
-    else:
-      return STATUS_UNKNOWN
-
-  except:
-    print(f"Exception occurred in reading '{in_file}'")
-    return STATUS_ERROR
+def get_file_length(in_file: str):
+  return AudioHelper.get_music_length(in_file)
 
 if __name__ == "__main__":
   parser = optparse.OptionParser(usage="cmd [optons] ..]")
@@ -82,35 +24,19 @@ if __name__ == "__main__":
   (options, args) = parser.parse_args()
 
   file_names = get_files_in_folder(options.folder,
-                                   file_extensions=AUDIO_EXTENSIONS,
+                                   file_extensions=AudioHelper.AUDIO_EXTENSIONS,
                                    resursive=True)
   file_names = list(file_names)
   print(f"There are {len(file_names)} files found.")
 
-  durations = Pool().map(get_music_length, file_names)
+  out = Pool().map(convert, file_names)
+  failed_files = [in_file for in_file, out_file in out if out_file is None]
+  print(f"#exception files: {len(failed_files)}")
+  print("\n".join(failed_files), file=open("errors.txt", "w"))
 
-  total_seconds = defaultdict(float)
-  error_file_num = 0
-  error_files = []
-  for idx, (file_name, seconds) in enumerate(zip(file_names, durations)):
-    if seconds < 0:
-      if seconds in [STATUS_ERROR, STATUS_UNKNOWN]:
-        error_file_num += 1
-        error_files.append(file_name)
-
-      continue
-
-    time_str = seconds_to_str(seconds)
-    # print(f"'{idx}:{file_name}': {time_str}")
-
-    total_seconds[get_file_extension(file_name)] += seconds
-
-  print(f"#exception files: {error_file_num}")
-  print("\n".join(error_files), file=open("errors.txt", "w"))
-
-  print(f"There are {len(file_names)} files found.")
-  for ext in total_seconds:
-    seconds = total_seconds[ext]
-    time_str = seconds_to_str(seconds)
-    print(f"type='{ext}', #time: {time_str}")
+  good_files = [out_file for in_file, out_file in out if out_file is not None]
+  all_lengths = Pool().map(get_file_length, good_files)
+  print(all_lengths)
+  total_seconds = sum(all_lengths)
+  print(f"total length: {AudioHelper.seconds_to_str(total_seconds)}")
 
