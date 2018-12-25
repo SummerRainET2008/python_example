ecf35fe15bd68a07ae233af12cf07d1759a1d100
Tian Xia <SummerRainET2008@gmail.com>
2018-12-25 11:38:52 -0800

    modified:   examples/sound/exam_audio_info.py   modified:   main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 #coding: utf8
 #author: Tian Xia (summer.xia1@pactera.com)
 
-from algorithm_3x import *
+import common as nlp
+import optparse
+import os
 
 if __name__ == "__main__":
-  os.system("clear")
-
   parser = optparse.OptionParser(usage="cmd [optons] ..]")
   #parser.add_option("-q", "--quiet", action="store_true", dest="verbose",
                      #default=False, help="")
